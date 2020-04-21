d45c882568eef4aaf77b16f15ad829e7a92e43c1
fonsecadh <UO258318@uniovi.es>
2020-04-21 22:54:26 +0200

    jaxb2-maven-plugin - No se ejecuta correctamente.

---
+++ sdi1920-408-lab-soap-sw/src/main/java/com/example/demo/Sdi1920408LabSoapSwApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi1920408LabSoapSwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi1920408LabSoapSwApplication.class, args);
+	}
+
+}
